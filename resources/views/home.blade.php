fe7971a261b8e4a4ff243a502d3af24826d26b79
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-05-11 15:43:16 +0200

    un po' di style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,19 @@
 @section('title', 'Home page')
 
 @section('content')
-
-    <h1>Elenco dei films</h1>
-
-    <ul>
+    <div class="row">
         @foreach ($movies as $movie)
-        <li>{{$movie->id}} - {{$movie->title}} - {{$movie->original_title}}</li>
+            <div class="col-3">
+                <div class="card m-3">
+                    <ul class="list-group list-group-flush">
+                        <div class="card-header"><strong>Titolo:</strong> {{ $movie->title }}</div>
+                        <li class="list-group-item"><strong>Titolo Originale: </strong> {{ $movie->original_title }}</li>
+                        <li class="list-group-item"><strong>Nazionalità: </strong>{{ $movie->nationality }}</li>
+                        <li class="list-group-item"><strong>Data di uscita: </strong>{{ $movie->date }}</li>
+                        <li class="list-group-item"><strong>Voto: </strong>{{ $movie->vote }}</li>
+                    </ul>
+                </div>
+            </div>
         @endforeach
-    </ul>
-
+    </div>
 @endsection
